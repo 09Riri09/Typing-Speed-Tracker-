6c91e35a6453ceaf9b9b9a99580ba2339c6bbcc4
09Riri09 <rares.dascalescu@gmail.com>
2024-03-12 13:10:18 +0200

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import {faker} from '@faker-js/faker';
+
+
+const words = faker.random.words(30);
+
+
+function App() {
+  return (
+    <>
+    <CountdownTimer timeLeft={30}/>
+    <GenerateWords words = {words}/>
+    </> 
+  );
+}
+
+
+const GenerateWords = ({words}:{words:string})=>{
+  return <div className = "text-4xl text-center text-slate-500">{words}</div>;
+};
+
+const CountdownTimer = ({timeLeft}: {timeLeft: number}) => 
+{
+return <h2 className = "text-primary-400 font-medium">Time: {timeLeft}</h2>
+};
+export default App;
+
---
+++ src/components/RestartButton.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { useRef } from 'react';
+import { MdRefresh } from "react-icons/md";
+
+function RestartButton() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default RestartButton
